448587e836bcb4532b2c219bce216c98c59bcd8c
skrrrrrrrr <kovid23arora@gmail.com>
2025-01-08 22:45:21 +0530

    fullscreeen

--- home-page.js
+++ home-page.js
@@ -1,2 +1,14 @@
-
+document.querySelectorAll('.nation').forEach(div => {
+    div.addEventListener('click', () => {
+      // Remove all other .nation divs
+      document.querySelectorAll('.nation').forEach(d => {
+        if (d !== div) {
+          d.remove();  // Remove the other divs
+        }
+        // div.remove
+      });
+      div.classList.add('scale-up-center');
+    });
+  });
+  
   
